127f2ba5c5802bf4d5aff96e7ad5db7b19f25c66
AliMohamedAbdelraheem <142576973+AliMohamedAbdelraheem@users.noreply.github.com>
2026-01-31 16:34:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function scrollToSection(id) {
+document.querySelectorAll(".nav-cards button").forEach(btn => {
+    btn.addEventListener("click", () => {
+      const target = document.getElementById(btn.dataset.target);
+      if (target) {
+        target.scrollIntoView({ behavior: "smooth" });
+      }
+    });
+  });
+  function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({
         behavior: "smooth"
     });
