00555ab927ecb5356fd90ffbc99f85ec9ce520ce
AliMohamedAbdelraheem <142576973+AliMohamedAbdelraheem@users.noreply.github.com>
2026-01-31 12:30:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const faders = document.querySelectorAll('.fade');
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    }
-  });
-});
-
-faders.forEach(el => observer.observe(el));
+document.body.style.opacity = "1";
